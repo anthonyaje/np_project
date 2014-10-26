a7a7d2df2cf0a669827f2f54867cdc7028d45b23
Anthony Ang <anthonyang86@gmail.com>
2014-10-27 02:04:52 +0800

    update: temp pipe to store intermediate stdout, read old one concate and store back

--- codes/server.c
+++ codes/server.c
@@ -11,6 +11,7 @@
 
 #define PORTNUM 8867
 #define BUFFER_SIZE 256
+#define MAX_LINE_CHAR 100
 
 void err_dump(char* str){
 	perror(str);
@@ -21,6 +22,7 @@ int print_ip(struct sockaddr_in* cli_addr);
 void parse_command(char* command, int sockfd);
 int num_of_pipe(char* line);
 void process1(char* command, int* arr); //move the pipe number from command into arr
+void exec_comm(char* token);
 
 int main (int argc, char* argv[]){
 	int socketfd, newsocketfd, cli_addr_size, childpid;
@@ -112,6 +114,7 @@ void parse_command(char* command,int sockfd){
 	int** pipefd;
 	char* instruct;
 	int* n_arr;
+	int* dirty_bit;    
 
 	n_pipe = num_of_pipe(command);
 	pipefd = malloc(n_pipe * sizeof(int*));
@@ -121,6 +124,7 @@ void parse_command(char* command,int sockfd){
 	}
 
 	n_arr = malloc((n_pipe)*sizeof(int));
+	dirty_bit = (int*) calloc((n_pipe),sizeof(int));  // 1:written 0:clean
 	pcount=0; 					//count the order of command
 	
 	process1(command, n_arr); 	//process the pipe n into array
@@ -129,11 +133,14 @@ void parse_command(char* command,int sockfd){
 	instruct = strtok(command,"|\r\n");
 	while(instruct != NULL){
 	printf("instruct %d: %s\n",pcount,instruct);
-//		printf("line: %s#\n",line);
 		pid = fork();
 		if(pid==0){ //child
 			char* token;
+			int temp_pipe[2];
+			pipe(temp_pipe);
+
 			if(pcount==0){ 			//first instruction
+				dirty_bit[pcount]=1;
 		   		close(pipefd[pcount][0]); 
 		   		dup2(pipefd[pcount][1],1);
 		   		dup2(pipefd[pcount][1],2);
@@ -141,60 +148,58 @@ void parse_command(char* command,int sockfd){
 			}
 			else{
 				close(pipefd[pcount][0]); 		//close reading end in the child
+				//prev pipe
+				if(dirty_bit[pcount-1]==0){ 				//the pipe is clean
+					//FIXME
+					// no need to read from pipe, stdin redirect to??
+					dup2(pipefd[pcount-1][0],0);	//direct the stdin from previous pipefd
+				}
+				else{							//the pipe is dirty
+					//TODO: dirty pipe need to be process: copy, concatenate, writeback
+					dup2(pipefd[pcount-1][0],0);	//direct the stdin from previous pipefd
+											
+				}
+				//next pipe
+				if(dirty_bit[pcount + n_arr[pcount] - 1] == 0){
+		   			dup2(pipefd[pcount + n_arr[pcount] - 1][1],1);		//direct the stdout to the pipefd[pcount]
+		   			dup2(pipefd[pcount + n_arr[pcount] - 1][1],2);		//direct the stderr to the pipefd[pcout]
+				}
+				else{
+					//TODO: dirty pipe need to be process: copy, concatenate, writeback
+					//close(temp_pipe[0]);
+					dup2(temp_pipe[1],1);
+					dup2(temp_pipe[1],2);
+					close(temp_pipe[1]);
+				}
+		   		close(pipefd[pcount][1]);		//wont need this write description 
+				
+				/*
+				close(pipefd[pcount][0]); 		//close reading end in the child
 				dup2(pipefd[pcount-1][0],0);	//direct the stdin from previous pipefd
 		   		dup2(pipefd[pcount + n_arr[pcount] - 1][1],1);		//direct the stdout to the pipefd[pcount]
 		   		dup2(pipefd[pcount + n_arr[pcount] - 1][1],2);		//direct the stderr to the pipefd[pcout]
 		   		close(pipefd[pcount][1]);		//wont need this write description 
+				*/
 			}
+		
 			token = strtok(instruct," \r\n");
 			while(token != NULL){				//FIXME "if" should be enough
 				printf("token: ##%s##\n",token);
-				if(strcmp(token,"ls")==0){
-					execlp(token,token,0,0);
-
-				}else if(strcmp(token,"cat")==0){
-					char* args[2];
-					args[0] = token;
-					args[1] = strtok(NULL," \r\n");
-					execlp(args[0],args[0],args[1],NULL);
-
-				}else if(strcmp(token,"printenv")==0){
-					char* args[2];
-					args[0] = token;
-					args[1] = strtok(NULL," \r\n");
-					execlp(args[0],args[0],args[1],NULL);
-					
-				}else if(strcmp(token,"setenv")==0){
-					//printf("here is setenv\n");
-					char* args[3];
-					args[0] = token;
-					args[1] = strtok(NULL," \r\n");
-					args[2] = strtok(NULL," \r\n");
-					//printf("arg012 %s %s %s\n",args[0],args[1],args[2]);
-					execlp(args[0],args[0],args[1],args[2],NULL);
-				
-				}else if(strcmp(token,"number")==0){
-					char* args[2];
-					args[0] = token;
-					args[1] = strtok(NULL," \r\n");
-					//execlp(args[0],args[0],args[1],NULL);
-					execl("./number",args[0],args[1],NULL);
-				}else if(strcmp(token,"noop")==0){
-					execlp(token,token,NULL);
-				}else if(strcmp(token,"removetag")==0){
-					char* args[2];
-					args[0] = token;
-					args[1] = strtok(NULL," \r\n");
-					//execlp(args[0],args[0],args[1],NULL);
-					execl("./removetag",args[0],args[1],NULL);
-				}else if(strcmp(token,"removetag0")==0){
-					execlp(token,token,NULL);
-				}
-				else{
-					printf("Unknown Command: %s\n",token);
-				}	
+				exec_comm(token);				//compare the string and execute it
 				token = strtok(NULL," \r\n");
 			}
+			
+			if((pcount!=0) && (dirty_bit[pcount + n_arr[pcount] - 1]!=0)){
+				//new pipe, write stdout.. read old data to buffer and append with the new datam write back to the old pipe			
+				char t1_buff[MAX_LINE_CHAR]={};
+				char t2_buff[MAX_LINE_CHAR]={};
+				read(pipefd[pcount + n_arr[pcount] - 1][0], t1_buff, MAX_LINE_CHAR);
+				read(temp_pipe[0], t2_buff, MAX_LINE_CHAR);
+				close(temp_pipe[0]);
+				strcat(t1_buff,t2_buff);
+				write(pipefd[pcount + n_arr[pcount] - 1][0], t1_buff, MAX_LINE_CHAR);
+			}	
+
 			_exit(EXIT_FAILURE);
 		}
 		else if(pid<0){ 
@@ -252,3 +257,50 @@ void process1(char* command, int* arr){
 		}
 	}
 }
+
+void exec_comm(char* token){
+	if(strcmp(token,"ls")==0){
+		execlp(token,token,0,0);
+
+	}else if(strcmp(token,"cat")==0){
+		char* args[2];
+		args[0] = token;
+		args[1] = strtok(NULL," \r\n");
+		execlp(args[0],args[0],args[1],NULL);
+
+	}else if(strcmp(token,"printenv")==0){
+		char* args[2];
+		args[0] = token;
+		args[1] = strtok(NULL," \r\n");
+		execlp(args[0],args[0],args[1],NULL);
+
+	}else if(strcmp(token,"setenv")==0){
+		char* args[3];
+		args[0] = token;
+		args[1] = strtok(NULL," \r\n");
+		args[2] = strtok(NULL," \r\n");
+		//printf("arg012 %s %s %s\n",args[0],args[1],args[2]);
+		execlp(args[0],args[0],args[1],args[2],NULL);
+
+	}else if(strcmp(token,"number")==0){
+		char* args[2];
+		args[0] = token;
+		args[1] = strtok(NULL," \r\n");
+		//execlp(args[0],args[0],args[1],NULL);
+		execl("./number",args[0],args[1],NULL);
+	}else if(strcmp(token,"noop")==0){
+		execlp(token,token,NULL);
+	}else if(strcmp(token,"removetag")==0){
+		char* args[2];
+		args[0] = token;
+		args[1] = strtok(NULL," \r\n");
+		//execlp(args[0],args[0],args[1],NULL);
+		execl("./removetag",args[0],args[1],NULL);
+	}else if(strcmp(token,"removetag0")==0){
+		execlp(token,token,NULL);
+	}
+	else{
+		printf("Unknown Command: %s\n",token);
+	}
+
+}	
--- codes/test/test.c
+++ codes/test/test.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <string.h>
 
 int num_of_pipe(char* line){
     int i,n;
@@ -12,41 +14,21 @@ return n+1;
 }
 
 int main(){
-char a[] = "c1 |2 c2 | c3 \r\n";
-char* token;
-char* pch;
+char a[10] = "abc";
+char token[15]={};
 int i,j;
-int arr[100]={};
 
-puts(a);
+token[0]='x';
+token[1]='y';
+token[2]='z';
 
-printf("numeber of pipe+1: %d\n",num_of_pipe(a));
+strcat(token,a);
 
-char* it=a;
-j=0;
-for(i=0; i<strlen(a); i++){
-	if(*(it+i) == '|'){
-		if( *(it+i+1) == ' ')
-			arr[j++] = 1;
-		else{
-			arr[j++] = atoi(it+i+1);
-		    *(it+i+1) = ' ';
-		}
-	}
+//puts(token);
+
+for(i=0; i<10; i++){
+	printf("%c",token[i]);
 }
-
-puts(a);
-
-for(i=0;i<10;i++){
-    printf("arr: %d\n", arr[i]);
-}
-
-pch = strchr(a,'|');
-printf("ooo%dooo\n",pch-a+1);
-
-pch = strchr(pch+1,'|');
-printf("ooo%dooo\n",pch-a+1);
-
 
 
 return 0;
